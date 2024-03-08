7ca8d89a587ae879c3d1d0c34ad5d3bd01930e83
hazalsigic <hazalsigic@hotmail.com>
2024-03-08 23:23:36 +0000

    question generation

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,14 @@
 var startButton = document.querySelector("#start");
 var timeDisplay = document.querySelector("#time");
 var startDisplay = document.querySelector("#start-screen");
-var questionDisplay = document.querySelector("#questions")
+var questionScreen = document.querySelector("#questions")
+var displayedQuestion = document.querySelector("#question-title")
+var choicesScreen = document.querySelector("#choices")
 var currentTime;
+var questionIndex = 0
+
 
 //function to start the timer when start button clicked
-
 function startTimer() {
     var currentTime = 75; 
     timeDisplay.textContent = currentTime;
@@ -21,8 +24,24 @@ function startTimer() {
 
 startButton.addEventListener("click",function(){
     startDisplay.classList.add("hide");
-    questionDisplay.classList.remove("hide");
+    questionScreen.classList.remove("hide");
     startTimer();
-});
+    displayQuestion(questionIndex);
+})
 
+function displayQuestion(questionIndex) {
+    var question = quizQuestions[questionIndex];
+    displayedQuestion.textContent = question.question;
+    choicesScreen.innerHTML = "";
 
+    question.choices.forEach((choice) => {
+        var button = document.createElement("button");
+        button.textContent = choice;
+        choicesScreen.appendChild(button);
+        button.classList.add("choice-button");
+
+        button.addEventListener("click", function() {
+            checkAnswer(choice, question.answer);
+        });
+    });
+}
